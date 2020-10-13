d91bd1509a9186ce537eff8d82ae9a6e6d9430f2
xoseo <41874503+xoseo@users.noreply.github.com>
2020-10-13 19:07:05 +0300

    Update is_free.py
    Предыдущий метод нам не подходит. Нужно Использовать permutations для поиска всех сочетаний
    Также исправил ошибку в последовательности убрав лишний нуль в цифрах

--- scripts/is_free.py
+++ scripts/is_free.py
@@ -1,17 +1,17 @@
 import whois, time
-from itertools import combinations_with_replacement
+from itertools import permutations
 
 def domain_generator(sequence, r):
 	# Передаем функции последовательность символов, диапазон(длину)
 	# Возвращает список полученных имен
-	# 01234567890abcdefghijklmnopqrstuvwxyz
-	dom_combinations = combinations_with_replacement(sequence, r)
+	# 0123456789abcdefghijklmnopqrstuvwxyz
+	dom_combinations = permutations(sequence, r)
 	list_of_domains = [''.join(i) for i in dom_combinations]
 	return list_of_domains
 
 
 if __name__ == "__main__":
-	dom_list = domain_generator('01234567890abcdefghijklmnopqrstuvwxyz', 2)
+	dom_list = domain_generator('0123456789abcdefghijklmnopqrstuvwxyz', 2)
 	for dom in dom_list:
 		with open('2_char_domains', 'a', encoding='utf-8') as f:
 			domain = whois.query(dom + '.ru')
