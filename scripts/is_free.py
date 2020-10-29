00dc3db4a2b1d8b4d249f57f6ecdb4bccb465b21
xoseo <ixoseo@gmail.com>
2020-10-29 17:21:51 +0300

    Если скрипт прервался, начинаем с последнего места

--- scripts/is_free.py
+++ scripts/is_free.py
@@ -1,20 +1,25 @@
-import whois, time
+import whois, time, os
 from itertools import permutations
 
 def domain_generator(sequence, r):
 	# Передаем функции последовательность символов, диапазон(длину)
 	# Возвращает список полученных имен
-	# 0123456789abcdefghijklmnopqrstuvwxyz
+	# -0123456789abcdefghijklmnopqrstuvwxyz
 	dom_combinations = permutations(sequence, r)
 	list_of_domains = [''.join(i) for i in dom_combinations]
 	return list_of_domains
 
-
-if __name__ == "__main__":
-	dom_list = domain_generator('0123456789abcdefghijklmnopqrstuvwxyz', 2)
-	for dom in dom_list:
-		with open('2_char_domains', 'a', encoding='utf-8') as f:
-			domain = whois.query(dom + '.ru')
+def domain_checker(dom_list, log_file):
+	if os.path.exists(log_file):
+		with open(log_file, "rb") as file:
+			file.seek(-2, os.SEEK_END)
+			while file.read(1) != b'\n':
+				file.seek(-2, os.SEEK_CUR)
+			dom = file.readline().decode().split()[0].strip('.ru')
+			start_from = dom_list.index(dom)+1
+	for dom in dom_list[start_from:]:
+		domain = whois.query(dom + '.ru')
+		with open(log_file, 'a', encoding='utf-8') as f:
 			if not domain:
 				print(f"{dom}.ru Expiration date: FREE")
 				f.write(f"{dom}.ru Expiration date: FREE\n")
@@ -22,3 +27,8 @@ if __name__ == "__main__":
 				print(f"{domain.__dict__['name']} Expiration date: {domain.__dict__['expiration_date']}")
 				f.write(f"{domain.__dict__['name']} Expiration date: {domain.__dict__['expiration_date']}\n")
 			time.sleep(0.5)
+
+
+if __name__ == "__main__":
+	dom_list = domain_generator('-0123456789abcdefghijklmnopqrstuvwxyz', 4)
+	domain_checker(dom_list, '/home/konta/4_char_domains')
